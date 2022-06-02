abf1aa4201f89d896ef002c7f1a105fe7fdd8b94
Sanidhya06 <sanidhyasoni655@gmail.com>
2022-06-02 21:05:39 +0530

    Mine

--- asd/backtraking/NQueens.java
+++ asd/backtraking/NQueens.java
@@ -1,7 +1,6 @@
 package asd.backtraking;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class NQueens
--- asd/recurssion/BinarySearch.java
+++ asd/recurssion/BinarySearch.java
@@ -1,4 +1,4 @@
-package asd.sorting;
+package asd.recurssion;
 
 public class BinarySearch
 {
