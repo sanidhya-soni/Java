ca768297d6b660c2dd6fa6d4d8c3e5b4f379c829
Sanidhya06 <sanidhyasoni655@gmail.com>
2022-06-17 17:40:57 +0530

    Me

--- asd/arrays/Merge2SortedArrays.java
+++ asd/arrays/Merge2SortedArrays.java
@@ -21,6 +21,46 @@ public class Merge2SortedArrays
             }
             nums1[j + 1] = temp;
         }
+
+        /* int arr[] = new int[m + n];
+        
+        int i = 0, j = 0, k = 0;
+        
+        while(i < m && j < n)
+        {
+            if(nums1[i] < nums2[j])
+            {
+                arr[k] = nums1[i];
+                k++;
+                i++;
+            }
+            else
+            {
+                arr[k] = nums2[j];
+                k++;
+                j++;
+            }
+        }
+        
+        
+        while(i < m)
+        {
+            arr[k] = nums1[i];
+            k++;
+            i++;
+        }
+        
+        while(j < n)
+        {
+            arr[k] = nums2[j];
+            k++;
+            j++;
+        }
+        
+        for(i = 0; i < m + n; i++)
+        {
+            nums1[i] = arr[i];
+        } */
     }
 
     
--- asd/arrays/RemoveDuplicatesFromSortedArray.java
+++ asd/arrays/RemoveDuplicatesFromSortedArray.java
@@ -22,12 +22,9 @@ public class RemoveDuplicatesFromSortedArray
         }
         return nums.length - x + 1;
     }
-<<<<<<< HEAD
-=======
 
     public static void main(String[] args)
     {
         
     }
->>>>>>> 42c08e32b99c03f60208e6b6b38165a5da3de7a4
 }
