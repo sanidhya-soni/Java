71ac9ad2d7216c73f6eec01be68c73b22ae8aea8
Sanidhya06 <sanidhyasoni655@gmail.com>
2022-07-08 10:06:51 +0530

    Me

--- asd/trees/Source.java
+++ asd/trees/Source.java
@@ -1,14 +1,15 @@
 package asd.trees;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Source
 {
+    Scanner sc = new Scanner(System.in);
 
     Node createBTree(Node root)
     {
-        Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter Data: ");
         int data = sc.nextInt();
 
@@ -25,11 +26,114 @@ public class Source
         return root;
     }
 
+    Node createFromLevelOrder()
+    {
+        Queue<Node> q = new ArrayDeque<Node>();
+        System.out.print("Enter data for root: ");
+        Node root = new Node(sc.nextInt());
+        int x;
+        q.add(root);
+
+        while(!q.isEmpty())
+        {
+            Node temp = q.poll();
+
+            System.out.print("Enter left side of " + temp.data + ": ");
+            x = sc.nextInt();
+            if(x != -1)
+            {
+                temp.left = new Node(x);
+                q.add(temp.left);
+            }
+
+            System.out.print("Enter right side of " + temp.data + ": ");
+            x = sc.nextInt();
+            if(x != -1)
+            {
+                temp.right = new Node(x);
+                q.add(temp.right);
+            }
+        }
+        return root;
+    }
+
+    void levelOrderTraversal(Node root)
+    {
+        Node newNode = new Node(-50000);
+        Queue<Node> q = new ArrayDeque<Node>();
+        if(root != null)
+        {
+            q.add(root);
+            q.add(newNode);
+        }
+
+        while(!q.isEmpty())
+        {
+            Node temp = q.poll();
+
+            if(temp.data == -50000)
+            {
+                System.out.println();
+                if(!q.isEmpty())
+                    q.add(newNode);
+            }
+            else
+            {
+                System.out.print(temp.data + " ");
+
+                if(temp.left != null)
+                    q.add(temp.left);
+                
+                if(temp.right != null)
+                    q.add(temp.right);
+            }
+        }
+    }
+
+    void inOrder(Node node)
+    {
+        if(node == null)
+            return;
+        inOrder(node.left);
+        System.out.print(node.data + " ");
+        inOrder(node.right);
+    }
+
+    void preOrder(Node node)
+    {
+        if(node == null)
+            return;
+        System.out.print(node.data + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    void postOrder(Node node)
+    {
+        if(node == null)
+            return;
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.data + " ");
+    }
+
     public static void main(String[] args)
     {
         Source ob = new Source();
 
         Node root = null;
-        ob.createBTree(root);
+        // root = ob.createBTree(root);
+        root = ob.createFromLevelOrder();
+        ob.inOrder(root);
+        System.out.println();
+        ob.preOrder(root);
+        System.out.println();
+        ob.postOrder(root);
+        System.out.println();
+        ob.levelOrderTraversal(root);
     }
 }
+
+
+// 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+// 1 2 4 -1 -1 5 -1 -1 3 -1 -1
