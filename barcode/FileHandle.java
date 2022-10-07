f26b9bf0ea209579dfb3fc123ae1dca54ea7105f
Sanidhya06 <sanidhyasoni655@gmail.com>
2022-10-07 18:59:02 +0530

    Me

---
+++ barcode/FileHandle.java
@@ -0,0 +1,9 @@
+package barcode;
+
+public class FileHandle
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
--- sani/Blah.java
+++ sani/Blah.java
@@ -10,3 +10,43 @@ public class Blah
         System.out.println(s.isEmpty());
     }
 }
+
+//public static int twoStacks(int maxSum, List<Integer> a, List<Integer> b) {
+//
+//    int a_index = 0;
+//    int b_index = 0;
+//    int count = 0;
+//    int sum = 0;
+//
+//    while (b_index < b.size() && sum + b.get(b_index) <= maxSum)
+//    {
+//        sum += b.get(b_index);
+//        b_index++;
+//    }
+//    count = b_index;
+//    b_index--;
+//
+//    while (a_index < a.size() && b_index < b.size())
+//    {
+//        sum += a.get(a_index);
+//        if (sum > maxSum)
+//        {
+//            while (b_index >= 0)
+//            {
+//                sum -= b.get(b_index);
+//                b_index--;
+//                if (sum <= maxSum)
+//                    break;
+//            }
+//            if (sum > maxSum && b_index < 0)
+//            {
+//                a_index--;
+//                break;
+//            }
+//        }
+//        count = Math.max(a_index + b_index + 2, count);
+//        a_index++;
+//    }
+//
+//    return count;
+//}
