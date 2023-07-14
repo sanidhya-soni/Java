6625c31a3af3f34ef019d98206836664c449aec8
Sanidhya Soni <sanidhyasoni655@gmail.com>
2023-07-14 10:48:00 +0530

    Me

---
+++ mypkg/SwitchCase.java
@@ -0,0 +1,90 @@
+package mypkg;
+
+import java.util.Scanner;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+        /*
+         * int r = 9;
+         * switch (r) {
+         * case 1:
+         * System.out.println("Case: " + 1);
+         * break;
+         * case 2:
+         * System.out.println("Case: " + 2);
+         * break;
+         * case 3:
+         * System.out.println("Case: " + 3);
+         * // break;
+         * case 4:
+         * System.out.println("Case: " + 4);
+         * // break;
+         * case 5:
+         * System.out.println("Case: " + 5);
+         * // break;
+         * case 6:
+         * System.out.println("Case: " + 6);
+         * // break;
+         * case 7:
+         * System.out.println("Case: " + 7);
+         * break;
+         * case 9:
+         * System.out.println("Case: " + 9);
+         * // break;
+         * case 8:
+         * System.out.println("Case: " + 8);
+         * break;
+         * default:
+         * System.out.println("Def");
+         * }
+         */
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter a = ");
+        // int a = sc.nextInt();
+        // System.out.print("Enter b = ");
+        // int b = sc.nextInt();
+        // System.out.print("Enter C = ");
+        // int c = sc.nextInt();
+
+        // if(a > b ) {
+        // if( a > c){
+        // System.out.println(a + " is greater");
+        // }
+        // else {
+        // System.out.println(c + " is greater");
+        // }
+        // }
+        // else {
+        // if(b > c){
+        // System.out.println(b + " is greater");
+        // }
+        // else {
+        // System.out.println(c + " is greater");
+        // }
+        // }
+        // sc.close();
+
+        double a = 2.2;
+        double b = 2.0;
+
+        double c = a / b;
+
+        System.out.println(c);
+
+        // while(true) {
+        // if(i <= 10) {
+        // System.out.println(i);
+        // } else {
+        // break;
+        // }
+        // i = i + 1;
+        // }
+    }
+}
+
+// 1
+// 2
+// 3
+// 4
+// 5
